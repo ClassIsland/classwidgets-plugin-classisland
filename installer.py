92d79818b842350a654be3ff1c3f76790ba70668
hello8693 <1320998105@qq.com>
2025-01-29 11:31:21 +0800

    添加操作系统支持检查，若不支持则显示错误消息并退出安装程序

--- installer.py
+++ installer.py
@@ -5,6 +5,7 @@ from PyQt5.QtWidgets import QWidget, QApplication
 from PyQt5.QtCore import Qt, QThread, pyqtSignal
 from .installer_ui import Ui_Form
 from .utils import get_classisland_path, download_and_extract_classisland
+from PyQt5.QtWidgets import QMessageBox
 
 class DownloadThread(QThread):
     progress = pyqtSignal(int)
@@ -19,6 +20,11 @@ class Installer(QWidget, Ui_Form):
         self.setWindowFlags(Qt.Window | Qt.WindowMinimizeButtonHint | Qt.CustomizeWindowHint)
         self.setFixedSize(self.size())
 
+        if not self.is_supported_os():
+            QMessageBox.critical(self, "不支持的操作系统", "该计算机不支持ClassIsland。")
+            self.close()
+            sys.exit(0)
+
         exe_path = os.path.join(get_classisland_path(), "ClassIsland.exe")
         if os.path.exists(exe_path):
             self.progressBar.setValue(100)
@@ -30,6 +36,12 @@ class Installer(QWidget, Ui_Form):
             self.download_thread.progress.connect(self.update_progress)
             self.download_thread.finished.connect(self.on_download_finished)
             self.download_thread.start()
+
+    def is_supported_os(self):
+        if sys.platform != "win32":
+            return False
+        version = sys.getwindowsversion()
+        return version.major >= 10
 
     def update_progress(self, value):
         self.progressBar.setValue(value)
